94366fa0194cb1795b9196145983aec8063d1d3f
cooper <tokyolabs@protonmail.com>
2022-05-02 22:31:56 -0400

    commit

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-let recent;
-
-chrome.runtime.onMessage.addListener(() => {
-  if (recent) {
-    recent.remove();
-    return true;
-  }
-});
-
-const getIt = () => {
-  document.onmousedown = e => {
-    recent = e.path[0];
-  };
-};
-
-var counter = 0;
-var myFunction = () => {
-  getIt();
-  counter += 1000;
-  if (counter <= 5000) {
-    timeout = setTimeout(myFunction, counter);
-  }
-};
-var timeout = setTimeout(myFunction, counter);
+var style = document.createElement('link');
+style.rel = 'stylesheet';
+style.type = 'text/css';
+style.href = chrome.extension.getURL('style.css');
+(document.head || document.documentElement).appendChild(style);
