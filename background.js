7a2ad88cdb0935ee563a004c014cc4dadab59e85
cooper <tokyolabs@protonmail.com>
2022-05-02 21:50:54 -0400

    commit

---
+++ background.js
@@ -0,0 +1,13 @@
+chrome.contextMenus.create(
+  {
+    title: 'Remove Element',
+    contexts: ['all'],
+    onclick: function (info) {
+      console.log(info);
+      chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+        chrome.tabs.sendMessage(tabs[0].id, { action: 'open_dialog_box' });
+      });
+    }
+  },
+  function () {}
+);
