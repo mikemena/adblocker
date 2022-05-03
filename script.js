d41ac2e0dd1620db50b9e6e64dec41f988f83948
cooper <tokyolabs@protonmail.com>
2022-05-03 16:07:27 -0400

    commit

--- script.js
+++ script.js
@@ -48,3 +48,46 @@ new PerformanceObserver(entryList => {
 
 // re-check as user scrolls
 document.addEventListener('scroll', () => getAds().forEach(hideAd));
+
+//delete cookies
+(function () {
+  'use strict';
+
+  window.addEventListener(
+    'message',
+    function (event) {
+      if (event.data.type && event.data.type === 'CLEAR_COOKIES_DOCUMENT') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+          var cookie = cookies[i].split('=');
+          var domainParts = document.domain.match(/[a-zA-Z0-9]+\.?/g);
+          for (var j = 0; j < domainParts.length - 1; j++) {
+            var domain =
+              (j > 0 ? '.' : '') +
+              document.domain
+                .match(/[a-zA-Z0-9]+\.?/g)
+                .splice(j)
+                .join('');
+            document.cookie =
+              cookie[0] +
+              '=;expires=Thu, 21 Sep 1979 00:00:01 UTC;domain=' +
+              domain +
+              ';';
+          }
+        }
+        window.postMessage({ type: 'COOKIES_CLEARED_USING_DOCUMENT' }, '*');
+      } else if (
+        event.data.type &&
+        event.data.type === 'CLEAR_COOKIES_EXTENSION_API'
+      ) {
+        chrome.runtime.sendMessage(
+          'CLEAR_COOKIES_EXTENSION_API',
+          function (response) {
+            window.postMessage({ type: response }, '*');
+          }
+        );
+      }
+    },
+    false
+  );
+})();
